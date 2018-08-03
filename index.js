0ee346952a9e6c51c3e0b0db7fffe07fb1afec4a
shawn prasad <shawnprasad92@gmail.com>
2018-08-03 23:41:00 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,26 @@
+function shout(string){
+  return string.toUpperCase()
+}
+
+function whisper(string){
+  return string.toLowerCase()
+}
+
+function logShout(string){
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string){
+  console.log(string.toLowerCase())
+}
+var greet = 'HELLO'
+function sayHiToGrandma(string){
+  console.log(string.toLowerCase())
+  return "I can't hear you!"
+ 
+}
+
+function sayHiToGrandma(string){
+  console.log(string.toLowerCase())
+  return "I can't hear you!"
+}
